e5f5c70cf9ee8ca896633f78a36eeedd5ea8961f
yunior <e_yunior@hotmail.com>
2019-03-27 12:21:34 -0700

    final

--- HP_Serialized_Parts/HP_Serialized_Parts.py
+++ HP_Serialized_Parts/HP_Serialized_Parts.py
@@ -1,12 +1,12 @@
 import  sys
+import  os
 import  logging
 import  time
 import  re
 import  pandas as pd
-from    sqlalchemy import create_engine
 import  smtplib
 from    email.message import EmailMessage
-from email.mime.multipart import MIMEMultipart
+from    email.mime.multipart import MIMEMultipart
 from    email.headerregistry import Address
 import  mimetypes
 from    email.mime.audio import MIMEAudio
@@ -14,11 +14,8 @@ from    email.mime.base import MIMEBase
 from    email.mime.image import MIMEImage
 from    email.mime.text import MIMEText
 from    email import encoders
+import  config
 
-#connect to SQL
-engine=create_engine("mssql+pyodbc://@WrkDSN")
-table_name='HP_Serialized_Parts'
-output_file="result.csv"
 
 def send_email(subject,content,to,files):
     msg = MIMEMultipart()
@@ -36,6 +33,7 @@ def send_email(subject,content,to,files):
     return 
 
 def attachment(fileToSend):
+    fn=os.path.basename(fileToSend)
     type = mimetypes.guess_type(fileToSend)
     ctype=type[0] if type[0] is not None else "application/octet-stream"
     encoding =type[1]
@@ -61,40 +59,47 @@ def attachment(fileToSend):
         attachment.set_payload(fp.read())
         fp.close()
         encoders.encode_base64(attachment)
-    attachment.add_header("Content-Disposition", "attachment", filename=fileToSend)
+    attachment.add_header("Content-Disposition", "attachment", filename=fn)
 
     return attachment
 
 if __name__ == '__main__':
 
-    try:
-        #subject=sys.argv[2] 
-        #files=sys.argv[4] 
-        #sender=sys.argv[6]
+    subject=sys.argv[2] 
+    files=sys.argv[4] 
+    sender=sys.argv[6]
 
-        #mfr= re.search("mfr='([a-z0-9]+)'",subject,re.RegexFlag.IGNORECASE).group(1)
+    try:
+        mfr= re.search("mfr=('[a-z0-9]+')",subject,re.RegexFlag.IGNORECASE).group(1)
 
         #Read file from email
-        ##parts:pd.DataFrame=pd.read_excel("C:/Users/Public/ScheduledScripts/Imap_Monitor/email_downloads/038/20190326T150237/HP Serialized Parts List.xlsx",'parts',index_col=False)
+        parts:pd.DataFrame=pd.read_excel(files,'parts',index_col=False)
 
         #Update parts table
-        ##parts.to_sql(con=engine,schema="dbo",if_exists="replace",index=False,name=table_name)
+        parts.to_sql(con=engine,schema="dbo",if_exists="replace",index=False,name=table_name)
 
-        ##query = "SET NOCOUNT ON;EXEC [dbo].[sp_HP_Serialized_Parts] @mfr = '{0}'".format('0007081791')
+        query = "SET NOCOUNT ON;EXEC [dbo].[sp_HP_Serialized_Parts] @mfr = {0}".format(mfr)
 
         #pull data from stored procedure
-        ##result= pd.read_sql_query(sql=query,con=engine)
-
-        ##result.to_csv(output_file)
+        result= pd.read_sql_query(sql=query,con=engine)
+        result.to_csv(output_file)
         
-        content = ""
-        subject = "None"
-        to = "yunior.rosellruiz@insight.com"
+        content = str(files)
+        subject = "Please see attached"
+        to = sender
         send_email(subject,content,to,(output_file,))
 
                    
     except Exception as e:
         logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
         logging.warning(str(e))
+
+        content = "Something went wrong, please reach out to the BI Team\n{}".format(str(e))
+        subject = "Error"
+        to = sender
+        send_email(subject,content,to,None)
+
+
+
         
 
